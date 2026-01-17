8ce505145d1ac008278606d896c933651afc9f8e
Mohamed Elbahja <bm9qdw5r@gmail.com>
2026-01-17 13:21:04 +0100

    helpers for sitemap/v3

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -1,18 +1,27 @@
 <?php
 namespace Melbahja\Seo\Utils;
 
+use Traversable,
+	IteratorAggregate;
+
+
 /**
  * @package Melbahja\Seo
- * @since v2.0
  * @see https://git.io/phpseo
  * @license MIT
- * @copyright Mohamed Elabhja
+ * @copyright Mohamed Elbahja
  */
 class Utils
 {
 
 	public static $encoding = 'UTF-8';
 
+	/**
+	 * Escape attr chars.
+	 *
+	 * @param  string $text
+	 * @return string
+	 */
 	public static function escape(string $text): string
 	{
 		return htmlspecialchars($text, ENT_QUOTES | ENT_HTML5, static::$encoding);
@@ -43,6 +52,50 @@ class Utils
 			['&amp;', '&apos;', '&quot;', '&gt;', '&lt;'],
 			$url['scheme'] . "://{$url['host']}{$url['path']}{$url['query']}"
 		);
+	}
+
+	/**
+	 * Encode url for sitemaps, other qoute/senstive chars are already escaped by xml writer!
+	 *
+	 * @param  string $url
+	 * @return string
+	 */
+	public static function encodeSitemapUrl(string $url): string
+	{
+		$url = parse_url($url);
+		$url['path']  = $url['path'] ?? '';
+		$url['query'] = $url['query'] ?? '';
+
+		if ($url['path'] !== '') {
+			$url['path'] = implode('/', array_map('rawurlencode', explode('/', $url['path'])));
+		}
+
+		if ($url['query'] !== '') {
+			$url['query'] = "?{$url['query']}";
+		}
+
+		return "{$url['scheme']}://{$url['host']}{$url['path']}{$url['query']}";
+	}
+
+	/**
+	 * Wrap callable func into a Traversable generator.
+	 *
+	 * @param  callable $func  must have yields
+	 * @return Traversable
+	 */
+	static function generator(callable $func): Traversable
+	{
+		return new class($func) implements IteratorAggregate
+		{
+			public function __construct(
+				private readonly mixed $callable
+			){}
+
+			public function getIterator(): Traversable
+			{
+				return ($this->callable)();
+			}
+		};
 	}
 }
 
